489388bdf7b449e00a9dd8c1afaf83d5694c062b
Brunonavarrooficial <bnacessoria@gmail.com>
2022-10-27 22:11:39 -0300

    Tailwind-Init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#1877F2',
+      },
+    },
+  },
+  plugins: [],
+}
