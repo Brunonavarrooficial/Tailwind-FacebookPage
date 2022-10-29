3e9223d10a4bbec920723d3ff7965cdfac61c714
Brunonavarrooficial <bnacessoria@gmail.com>
2022-10-29 19:46:01 -0300

    Container-Styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'facebook-blue': '#1877F2',
         'gray-background': '#F0F2F5',
         'text-second': '#0f172a',
+        'green-facebook': '#36A420',
       },
       spacing: {
         '580': '580px',
